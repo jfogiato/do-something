50273aef43259b4c91a17ed4744c2cd7059ff36f
Joe Fogiato <joe.fogiato@gmail.com>
2023-04-20 18:43:06 -0400

    Add: NotFound component with routing for all non-existent paths

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NotFound.css';
+
+
+const NotFound = () => {
+  return (
+    <section className='not-found'>
+      <h1>Dang. The page you're looking for has either moved or doesn't exist.</h1>
+      <h2>Try refreshing or heading home.</h2>
+    </section>
+  );
+}
+
+export default NotFound;

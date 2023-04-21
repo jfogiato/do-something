fcfb622d1e701251b65798e4f30b8cd8621a2ab5
Joe Fogiato <joe.fogiato@gmail.com>
2023-04-20 20:13:53 -0400

    Add: header logo with link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import { Link, Route, Switch } from 'react-router-dom';
+import logoImage from '../../images/DoSomething.png';
 
 const Header = () => {
   return (
@@ -8,7 +9,7 @@ const Header = () => {
       <Route exact path='/'></Route>
       <Route path='*'>
         <header data-cy='header'>
-          <Link to='/' className='link'>Do Something.</Link>
+          <Link to='/' className='link'><img className='logo-link'  src={logoImage}/></Link>
           <Link to='/i-want-to' className='link'>Do Something Else.</Link>
         </header>
       </Route>

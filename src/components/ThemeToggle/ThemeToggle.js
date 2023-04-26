49e70c9c666f5159905079ee4014336e8944315f
Joe Fogiato <joe.fogiato@gmail.com>
2023-04-26 10:18:03 -0400

    Fix: body theme in a sort of hacky but effective way

--- src/components/ThemeToggle/ThemeToggle.js
+++ src/components/ThemeToggle/ThemeToggle.js
@@ -4,11 +4,28 @@ import ThemeContext from '../../Contexts/ThemeContext';
 
 const ThemeToggle = ({ toggleTheme }) => {
   const theme = useContext(ThemeContext);
-
+  const body = document.body;
   const iconTheme = theme === 'dark' && 'toggle-icon-dark';
+  
+  body.classList.add('light');
+
+  const toggleThemeGlobal = () => {
+    toggleTheme();
+    toggleBody();
+  }
+
+  const toggleBody = () => {
+    if (theme === 'light') {
+      body.classList.add('dark');
+      body.classList.remove('light');
+    } else {
+      body.classList.add('light');
+      body.classList.remove('dark');
+    }
+  }
 
   return (
-    <button onClick={toggleTheme} className='toggle-icon'>
+    <button onClick={toggleThemeGlobal} className='toggle-icon'>
       <span className={`material-symbols-outlined ${iconTheme}`}>{theme === 'light' ? 'dark_mode' : 'light_mode'}</span>
     </button>
   );

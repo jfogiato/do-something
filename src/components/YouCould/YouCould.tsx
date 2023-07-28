95ad4743b3f614a47a48303b06578201bb0b3853
Joe Fogiato <joe.fogiato@gmail.com>
2023-07-28 19:34:27 -0400

    Update: YouCould component to add conditional component renderings

--- src/components/YouCould/YouCould.tsx
+++ src/components/YouCould/YouCould.tsx
@@ -1,19 +1,19 @@
-import { ReactElement, useContext } from 'react';
+import { useContext } from 'react';
 import './YouCould.css';
-import { Link } from 'react-router-dom';
 import ThemeContext from '../../Contexts/ThemeContext';
 import { Activity } from '../../models';
+import Loading from '../Loading/Loading';
+import NoMatch from '../NoMatch/NoMatch';
+import Match from '../Match/Match';
 
 interface YouCouldProps {
-  activityObject: Activity,
+  activityObject: Activity | undefined,
   addActivity: () => void,
   setFirst: (first: boolean) => void
 };
 
 const YouCould: React.FC<YouCouldProps> = ({ activityObject, addActivity, setFirst }) => {
-  
-  const { activity, link, error } = activityObject;
-  
+    
   const theme = useContext<string>(ThemeContext);
 
   const handleSubmit = () :  void => {
@@ -21,36 +21,15 @@ const YouCould: React.FC<YouCouldProps> = ({ activityObject, addActivity, setFir
     setFirst(false);
   }
 
-  const match: ReactElement =
-    <>
-      <h2>You could...</h2>
-      <div className='activity-container'>
-        <h3 data-cy='activity-name'>{activity}</h3>
-        {link && <a target="_blank" rel="noreferrer" href={link}>Link</a>}
-        <form className='activity-choose-form'>
-          <Link data-cy='do-it-btn' to='/you-did' onClick={handleSubmit} className='uni-btn'>Do it.</Link>
-          <Link data-cy='nah-btn' to='/i-want-to' className='uni-btn'>Nah.</Link>
-        </form>
-      </div>
-    </>
-  ;
-
-  const noMatch: ReactElement = 
-    <section className='activity-container'>
-      <p>Nothing to do given those contraints.</p>
-      <Link data-cy='back-btn' to='/i-want-to' className='uni-btn'>Try something else.</Link>
-    </section>
-  ;
-
-  const loading: ReactElement = 
-  <section className='activity-container'>
-    <p>Loading....</p>
-  </section>
-;
-
   return (
     <section className={`you-could ${theme}`}>
-      {activity ? match : error ? noMatch : loading}
+      {
+        !activityObject
+          ? <Loading/>
+          : activityObject.error
+            ? <NoMatch/>
+            : <Match activityData={activityObject} handleSubmit={handleSubmit}/>
+      }
     </section>
   );
 }

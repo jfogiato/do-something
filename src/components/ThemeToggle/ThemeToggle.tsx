b3a29ec917ad342a3e7c52482607d6dc69cfa673
Joe Fogiato <joe.fogiato@gmail.com>
2023-07-28 16:49:14 -0400

    Update: ThemeToggle component to TSX

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,20 +1,20 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import './ThemeToggle.css';
 import ThemeContext from '../../Contexts/ThemeContext';
 
-const ThemeToggle = ({ toggleTheme }) => {
-  const theme = useContext(ThemeContext);
-  const body = document.body;
-  const iconTheme = theme === 'dark' && 'toggle-icon-dark';
+const ThemeToggle: React.FC<{ toggleTheme: Function }> = ({ toggleTheme }) => {
+  const theme = useContext<string>(ThemeContext);
+  const body: HTMLElement = document.body;
+  const iconTheme: string | boolean = theme === 'dark' && 'toggle-icon-dark';
   
   body.classList.add('light');
 
-  const toggleThemeGlobal = () => {
+  const toggleThemeGlobal = () : void => {
     toggleTheme();
     toggleBody();
   }
 
-  const toggleBody = () => {
+  const toggleBody = () : void => {
     if (theme === 'light') {
       body.classList.add('dark');
       body.classList.remove('light');
